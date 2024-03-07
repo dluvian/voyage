20b01fa6eb82574d5e97574ef026dd9b25966a5b
dluvian <dluvian@proton.me>
2024-03-07 22:43:44 +0800

    Remove unused filter

--- app/src/main/java/com/dluvian/voyage/core/model/filter/EvalFilter.kt
+++ app/src/main/java/com/dluvian/voyage/core/model/filter/EvalFilter.kt
@@ -1,6 +1,6 @@
 package com.dluvian.voyage.core.model.filter
 
-sealed class EvalFilter
-data object Latest : EvalFilter()
+// TODO: sealed class EvalFilter
+// TODO: data object Latest : EvalFilter()
 // TODO: data object Top : EvalFilter()
 // TODO: data object Controversial : EvalFilter()
