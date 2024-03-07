97a32616754b29cc96cc1c7ba3fd9b1aaa03e851
dluvian <dluvian@proton.me>
2024-03-07 09:12:40 +0800

    Fix vote query

--- app/src/main/java/com/dluvian/voyage/data/room/dao/VoteUpsertDao.kt
+++ app/src/main/java/com/dluvian/voyage/data/room/dao/VoteUpsertDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
+import com.dluvian.voyage.core.EventIdHex
 import com.dluvian.voyage.core.PubkeyHex
 import com.dluvian.voyage.data.room.entity.VoteEntity
 
@@ -12,16 +13,19 @@ import com.dluvian.voyage.data.room.entity.VoteEntity
 interface VoteUpsertDao {
     @Transaction
     suspend fun upsertVote(voteEntity: VoteEntity) {
-        val newestCreatedAt = internalGetNewestCreatedAt(pubkey = voteEntity.pubkey) ?: 0L
+        val newestCreatedAt = internalGetNewestCreatedAt(
+            pubkey = voteEntity.pubkey,
+            postId = voteEntity.postId
+        ) ?: 0L
         if (voteEntity.createdAt < newestCreatedAt) return
 
-        internalInsertVoteOrReplace(voteEntity = voteEntity)
+        internalUpsertVote(voteEntity = voteEntity)
     }
 
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun internalInsertVoteOrReplace(voteEntity: VoteEntity)
+    suspend fun internalUpsertVote(voteEntity: VoteEntity)
 
-    @Query("SELECT MAX(createdAt) FROM vote WHERE pubkey = :pubkey")
-    suspend fun internalGetNewestCreatedAt(pubkey: PubkeyHex): Long?
+    @Query("SELECT MAX(createdAt) FROM vote WHERE pubkey = :pubkey AND postId = :postId")
+    suspend fun internalGetNewestCreatedAt(pubkey: PubkeyHex, postId: EventIdHex): Long?
 }
