a91bd68bc94d354bdc2f8222e0d278accdc35aff
dluvian <dluvian@proton.me>
2024-03-07 11:37:26 +0800

    Implement backpress

--- app/src/main/java/com/dluvian/voyage/ui/VoyageNavigator.kt
+++ app/src/main/java/com/dluvian/voyage/ui/VoyageNavigator.kt
@@ -1,7 +1,9 @@
 package com.dluvian.voyage.ui
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import com.dluvian.voyage.core.BackPress
 import com.dluvian.voyage.core.Core
 import com.dluvian.voyage.core.navigation.Home
 import com.dluvian.voyage.ui.view.HomeView
@@ -9,6 +11,8 @@ import com.dluvian.voyage.ui.view.HomeView
 @Composable
 fun VoyageNavigator(core: Core) {
     val currentView by core.navigator.currentView
+    BackHandler { core.onUIEvent(BackPress) }
+
     when (currentView) {
         is Home -> HomeView()
     }
