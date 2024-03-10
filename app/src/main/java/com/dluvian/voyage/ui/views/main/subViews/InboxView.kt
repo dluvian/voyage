08d10ad914d331a33604b7bc369a1c7fc1391666
dluvian <dluvian@proton.me>
2024-03-10 11:40:51 +0800

    Add "Coming soon" to inbox

--- app/src/main/java/com/dluvian/voyage/ui/views/main/subViews/InboxView.kt
+++ app/src/main/java/com/dluvian/voyage/ui/views/main/subViews/InboxView.kt
@@ -1,9 +1,36 @@
 package com.dluvian.voyage.ui.views.main.subViews
 
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.dluvian.voyage.ui.theme.DenimBlue
+import com.dluvian.voyage.ui.theme.TallPoppyRed
 
 @Composable
 fun InboxView() {
-    Text(text = "Inbox")
+    val infiniteTransition = rememberInfiniteTransition(label = "infinite transition")
+    val animatedColor by infiniteTransition.animateColor(
+        initialValue = TallPoppyRed,
+        targetValue = DenimBlue,
+        animationSpec = infiniteRepeatable(tween(1000), RepeatMode.Reverse),
+        label = "color"
+    )
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            style = MaterialTheme.typography.titleLarge,
+            text = "Coming soon", color = animatedColor
+        )
+    }
 }
