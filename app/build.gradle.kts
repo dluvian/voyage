90a2996539350218ae1163bd0f51395dee84df36
dluvian <dluvian@proton.me>
2024-03-13 14:46:56 +0800

    Minify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,11 +23,12 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
