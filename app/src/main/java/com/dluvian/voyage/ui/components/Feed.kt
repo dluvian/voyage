35f3fa53611a9333d5c057bbf1fc385ad33cc845
dluvian <dluvian@proton.me>
2024-03-18 15:36:50 +0800

    Remove unused indexing

--- app/src/main/java/com/dluvian/voyage/ui/components/Feed.kt
+++ app/src/main/java/com/dluvian/voyage/ui/components/Feed.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.LinearProgressIndicator
@@ -48,7 +48,7 @@ fun Feed(
     PullRefreshBox(isRefreshing = isRefreshing, onRefresh = onRefresh) {
         if (isAppending) LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
         LazyColumn(modifier = Modifier.fillMaxSize()) {
-            itemsIndexed(items = posts, key = { _, item -> item.id }) { i, post ->
+            items(items = posts, key = { item -> item.id }) { post ->
                 PostRow(
                     post = post,
                     onUpdate = onUpdate
