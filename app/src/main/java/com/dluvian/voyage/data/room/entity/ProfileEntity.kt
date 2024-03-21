3acf94438e8fa44b3838ce6947feea1ee2f27af7
dluvian <dluvian@proton.me>
2024-03-21 14:09:37 +0800

    Improve profile search speed

--- app/src/main/java/com/dluvian/voyage/data/room/entity/ProfileEntity.kt
+++ app/src/main/java/com/dluvian/voyage/data/room/entity/ProfileEntity.kt
@@ -1,6 +1,7 @@
 package com.dluvian.voyage.data.room.entity
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.dluvian.voyage.core.MAX_NAME_LEN
 import com.dluvian.voyage.core.PubkeyHex
@@ -8,7 +9,12 @@ import com.dluvian.voyage.data.event.ValidatedProfile
 import rust.nostr.protocol.Nip19Profile
 import rust.nostr.protocol.PublicKey
 
-@Entity(tableName = "profile")
+@Entity(
+    tableName = "profile",
+    indices = [
+        Index(value = ["name"])
+    ]
+)
 data class ProfileEntity(
     @PrimaryKey val pubkey: PubkeyHex,
     val name: String?,
