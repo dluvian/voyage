220ef2b97c9f0a9ed89f0ca50f4e44ba8158b2b9
dluvian <dluvian@proton.me>
2024-03-22 14:04:06 +0800

    Fix DiscoverViewActions impl

--- app/src/main/java/com/dluvian/voyage/core/viewModel/DiscoverViewModel.kt
+++ app/src/main/java/com/dluvian/voyage/core/viewModel/DiscoverViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.dluvian.voyage.core.DELAY_10SEC
 import com.dluvian.voyage.core.DELAY_1SEC
 import com.dluvian.voyage.core.DiscoverViewAction
 import com.dluvian.voyage.core.DiscoverViewFollowProfile
@@ -12,6 +13,8 @@ import com.dluvian.voyage.core.DiscoverViewInit
 import com.dluvian.voyage.core.DiscoverViewRefresh
 import com.dluvian.voyage.core.DiscoverViewUnfollowProfile
 import com.dluvian.voyage.core.DiscoverViewUnfollowTopic
+import com.dluvian.voyage.core.PubkeyHex
+import com.dluvian.voyage.core.Topic
 import com.dluvian.voyage.core.model.TopicFollowState
 import com.dluvian.voyage.data.interactor.ProfileFollower
 import com.dluvian.voyage.data.interactor.TopicFollower
@@ -19,6 +22,7 @@ import com.dluvian.voyage.data.model.FullProfile
 import com.dluvian.voyage.data.provider.ProfileProvider
 import com.dluvian.voyage.data.provider.TopicProvider
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
@@ -38,15 +42,35 @@ class DiscoverViewModel(
         when (action) {
             is DiscoverViewInit -> init()
             is DiscoverViewRefresh -> refresh()
-            is DiscoverViewFollowTopic -> topicFollower.follow(action.topic)
-            is DiscoverViewUnfollowTopic -> topicFollower.unfollow(action.topic)
-            is DiscoverViewFollowProfile -> profileFollower.follow(action.pubkey)
-            is DiscoverViewUnfollowProfile -> profileFollower.unfollow(action.pubkey)
+            is DiscoverViewFollowTopic -> updateTopicFollowState(
+                topic = action.topic,
+                isFollowed = true
+            )
+
+            is DiscoverViewUnfollowTopic -> updateTopicFollowState(
+                topic = action.topic,
+                isFollowed = false
+            )
+
+            is DiscoverViewFollowProfile -> updateProfileFollowState(
+                pubkey = action.pubkey,
+                isFollowed = true
+            )
+
+            is DiscoverViewUnfollowProfile -> updateProfileFollowState(
+                pubkey = action.pubkey,
+                isFollowed = false
+            )
         }
     }
 
+    private var initJob: Job? = null
     private fun init() {
-        if (popularTopics.value.isEmpty() && popularProfiles.value.isEmpty()) refresh()
+        if (initJob?.isActive == true) return
+        initJob = viewModelScope.launch(Dispatchers.IO) {
+            refresh()
+            delay(DELAY_10SEC)
+        }
     }
 
     private fun refresh() {
@@ -67,6 +91,23 @@ class DiscoverViewModel(
             joinAll(topicJob, profileJob)
         }.invokeOnCompletion {
             isRefreshing.value = false
+        }
+    }
+
+    private fun updateTopicFollowState(topic: Topic, isFollowed: Boolean) {
+        if (isFollowed) topicFollower.follow(topic) else topicFollower.unfollow(topic)
+        popularTopics.value = popularTopics.value.map {
+            it.copy(isFollowed = topicFollower.forcedStates.value[topic] ?: false)
+        }
+    }
+
+    private fun updateProfileFollowState(pubkey: PubkeyHex, isFollowed: Boolean) {
+        if (isFollowed) profileFollower.follow(pubkey) else profileFollower.unfollow(pubkey)
+        popularProfiles.value = popularProfiles.value.map {
+            val advanced = it.advancedProfile.copy(
+                isFriend = profileFollower.forcedFollows.value[pubkey] ?: false
+            )
+            it.copy(advancedProfile = advanced)
         }
     }
 }
