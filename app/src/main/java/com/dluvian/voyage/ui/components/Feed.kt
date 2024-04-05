a538cf879fb56fc506b1c80d0cc47d8dfacdec45
dluvian <dluvian@proton.me>
2024-04-05 15:05:00 +0800

    Improve next page button

--- app/src/main/java/com/dluvian/voyage/ui/components/Feed.kt
+++ app/src/main/java/com/dluvian/voyage/ui/components/Feed.kt
@@ -1,6 +1,10 @@
 package com.dluvian.voyage.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -9,11 +13,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.dluvian.voyage.R
 import com.dluvian.voyage.core.ComposableContent
 import com.dluvian.voyage.core.Fn
 import com.dluvian.voyage.core.OnUpdate
 import com.dluvian.voyage.core.model.IPaginator
 import com.dluvian.voyage.ui.components.indicator.FullLinearProgressIndicator
+import com.dluvian.voyage.ui.theme.spacing
 
 @Composable
 fun Feed(
@@ -40,8 +47,16 @@ fun Feed(
             }
             if (posts.isNotEmpty()) {
                 item {
-                    Button(onClick = onAppend) {
-                        Text(text = "Next Page") // TODO: Make it look good
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        Button(
+                            modifier = Modifier.padding(horizontal = spacing.screenEdge),
+                            onClick = onAppend
+                        ) {
+                            Text(text = stringResource(id = R.string.next_page))
+                        }
                     }
                 }
             }
