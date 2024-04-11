9ba254d34806feb8bbea8e7712507bab4439e14c
dluvian <dluvian@proton.me>
2024-04-11 18:55:24 +0800

    Simplify comment chip

--- app/src/main/java/com/dluvian/voyage/ui/components/chip/CommentChip.kt
+++ app/src/main/java/com/dluvian/voyage/ui/components/chip/CommentChip.kt
@@ -1,13 +1,17 @@
 package com.dluvian.voyage.ui.components.chip
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import com.dluvian.voyage.R
 import com.dluvian.voyage.core.Fn
@@ -17,19 +21,26 @@ import com.dluvian.voyage.ui.theme.spacing
 
 @Composable
 fun CommentChip(commentCount: Int, onClick: Fn) {
-    AssistChip(
+    Row(
         modifier = Modifier
-            .height(AssistChipDefaults.Height.times(0.8f))
+            .clip(RoundedChip)
+            .clickable(onClick = onClick)
             .padding(horizontal = spacing.large),
-        onClick = onClick,
-        leadingIcon = {
-            Icon(
-                modifier = Modifier.height(AssistChipDefaults.Height.times(0.5f)),
-                imageVector = CommentIcon,
-                contentDescription = stringResource(id = R.string.comment)
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            modifier = Modifier
+                .height(AssistChipDefaults.Height.times(0.6f))
+                .padding(horizontal = spacing.small),
+            imageVector = CommentIcon,
+            contentDescription = stringResource(id = R.string.comment),
+            tint = MaterialTheme.colorScheme.primary
+        )
+        if (commentCount > 0) {
+            Text(
+                modifier = Modifier.padding(horizontal = spacing.small),
+                text = commentCount.toString()
             )
-        },
-        shape = RoundedChip,
-        label = { Text(text = "$commentCount ${stringResource(id = R.string.comments)}") },
-    )
+        }
+    }
 }
