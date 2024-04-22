a541a27dd0c88c6fd21db3604bb5a6ce6ed265b5
dluvian <dluvian@proton.me>
2024-04-22 23:04:40 +0800

    imp: Make full post bottom sheet scrollable

--- app/src/main/java/com/dluvian/voyage/ui/components/bottomSheet/FullPostBottomSheet.kt
+++ app/src/main/java/com/dluvian/voyage/ui/components/bottomSheet/FullPostBottomSheet.kt
@@ -1,20 +1,34 @@
 package com.dluvian.voyage.ui.components.bottomSheet
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import com.dluvian.voyage.R
 import com.dluvian.voyage.core.Fn
 import com.dluvian.voyage.ui.components.text.AnnotatedText
+import com.dluvian.voyage.ui.theme.spacing
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FullPostBottomSheet(content: AnnotatedString, onDismiss: Fn) {
     ModalBottomSheet(onDismissRequest = onDismiss) {
         BottomSheetColumn(header = stringResource(id = R.string.original_post)) {
-            AnnotatedText(text = content, onClick = { })
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(state = rememberScrollState())
+                    .padding(bottom = spacing.xxl)
+            ) {
+                AnnotatedText(text = content, onClick = { })
+            }
         }
     }
 }
