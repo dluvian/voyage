3065fedc198599acc1fbfa18804d3c71ee61bf6b
dluvian <dluvian@proton.me>
2024-04-30 07:59:02 +0800

    wip: Follow cross posted author

--- app/src/main/java/com/dluvian/voyage/ui/components/dropdown/ParentRowDropdown.kt
+++ app/src/main/java/com/dluvian/voyage/ui/components/dropdown/ParentRowDropdown.kt
@@ -8,8 +8,10 @@ import androidx.compose.ui.res.stringResource
 import com.dluvian.nostr_kt.createNeventStr
 import com.dluvian.voyage.R
 import com.dluvian.voyage.core.DeletePost
+import com.dluvian.voyage.core.Fn
 import com.dluvian.voyage.core.FollowProfile
 import com.dluvian.voyage.core.OnUpdate
+import com.dluvian.voyage.core.SignerLauncher
 import com.dluvian.voyage.core.UnfollowProfile
 import com.dluvian.voyage.core.copyAndToast
 import com.dluvian.voyage.core.getSignerLauncher
@@ -17,6 +19,7 @@ import com.dluvian.voyage.core.model.FriendTrust
 import com.dluvian.voyage.core.model.IParentUI
 import com.dluvian.voyage.core.model.NoTrust
 import com.dluvian.voyage.core.model.Oneself
+import com.dluvian.voyage.core.model.RootPostUI
 import com.dluvian.voyage.core.model.WebTrust
 
 @Composable
@@ -31,32 +34,18 @@ fun ParentRowDropdown(
         expanded = isOpen,
         onDismissRequest = onDismiss
     ) {
-        when (parent.trustType) {
-            Oneself -> {}
-            FriendTrust -> {
-                SimpleDropdownItem(
-                    text = stringResource(id = R.string.unfollow),
-                    onClick = {
-                        onUpdate(
-                            UnfollowProfile(pubkey = parent.pubkey, signerLauncher = signerLauncher)
-                        )
-                        onDismiss()
-                    }
-                )
-            }
-
-            NoTrust, WebTrust -> {
-                SimpleDropdownItem(
-                    text = stringResource(id = R.string.follow),
-                    onClick = {
-                        onUpdate(
-                            FollowProfile(pubkey = parent.pubkey, signerLauncher = signerLauncher)
-                        )
-                        onDismiss()
-                    }
-                )
-            }
-        }
+        FollowItem(
+            parent = parent,
+            signerLauncher = signerLauncher,
+            onDismiss = onDismiss,
+            onUpdate = onUpdate
+        )
+        FollowCrossPostedItem(
+            parent = parent,
+            signerLauncher = signerLauncher,
+            onDismiss = onDismiss,
+            onUpdate = onUpdate
+        )
         val clip = LocalClipboardManager.current
         val context = LocalContext.current
         val idCopiedToast = stringResource(id = R.string.note_id_copied)
@@ -96,6 +85,84 @@ fun ParentRowDropdown(
                     onDismiss()
                 }
             )
+        }
+    }
+}
+
+@Composable
+private fun FollowItem(
+    parent: IParentUI,
+    signerLauncher: SignerLauncher,
+    onDismiss: Fn,
+    onUpdate: OnUpdate
+) {
+    when (parent.trustType) {
+        Oneself -> {}
+        FriendTrust -> {
+            SimpleDropdownItem(
+                text = stringResource(id = R.string.unfollow),
+                onClick = {
+                    onUpdate(
+                        UnfollowProfile(pubkey = parent.pubkey, signerLauncher = signerLauncher)
+                    )
+                    onDismiss()
+                }
+            )
+        }
+
+        NoTrust, WebTrust -> {
+            SimpleDropdownItem(
+                text = stringResource(id = R.string.follow),
+                onClick = {
+                    onUpdate(
+                        FollowProfile(pubkey = parent.pubkey, signerLauncher = signerLauncher)
+                    )
+                    onDismiss()
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun FollowCrossPostedItem(
+    parent: IParentUI,
+    signerLauncher: SignerLauncher,
+    onDismiss: Fn,
+    onUpdate: OnUpdate
+) {
+    if (parent is RootPostUI && parent.crossPostedTrustType != null && parent.crossPostedPubkey != null) {
+        when (parent.crossPostedTrustType) {
+            Oneself -> {}
+            FriendTrust -> {
+                SimpleDropdownItem(
+                    text = stringResource(id = R.string.unfollow_cross_posted_author),
+                    onClick = {
+                        onUpdate(
+                            UnfollowProfile(
+                                pubkey = parent.crossPostedPubkey,
+                                signerLauncher = signerLauncher
+                            )
+                        )
+                        onDismiss()
+                    }
+                )
+            }
+
+            NoTrust, WebTrust -> {
+                SimpleDropdownItem(
+                    text = stringResource(id = R.string.follow_cross_posted_author),
+                    onClick = {
+                        onUpdate(
+                            FollowProfile(
+                                pubkey = parent.crossPostedPubkey,
+                                signerLauncher = signerLauncher
+                            )
+                        )
+                        onDismiss()
+                    }
+                )
+            }
         }
     }
 }
