ea283e1d6e410df4f588610a4e31abe7834c9504
dluvian <dluvian@proton.me>
2024-05-04 19:55:12 +0800

    refac: Reset migration steps which were not part of last release

--- app/src/main/java/com/dluvian/voyage/data/room/AppDatabase.kt
+++ app/src/main/java/com/dluvian/voyage/data/room/AppDatabase.kt
@@ -42,17 +42,12 @@ import com.dluvian.voyage.data.room.view.RootPostView
 
 
 @Database(
-    version = 9,
+    version = 4,
     exportSchema = true,
     autoMigrations = [
         AutoMigration(from = 1, to = 2),
         AutoMigration(from = 2, to = 3),
         AutoMigration(from = 3, to = 4),
-        AutoMigration(from = 4, to = 5),
-        AutoMigration(from = 5, to = 6),
-        AutoMigration(from = 6, to = 7),
-        AutoMigration(from = 7, to = 8),
-        AutoMigration(from = 8, to = 9),
     ],
     entities = [
         PostEntity::class,
