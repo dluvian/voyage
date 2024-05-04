31315f372ad526b5839edb2e32ba937ed48aad92
dluvian <dluvian@proton.me>
2024-05-04 19:47:01 +0800

    chore: Upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,11 +73,11 @@ android {
 dependencies {
     implementation(project(":nostr-kt"))
 
-    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
 
-    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
