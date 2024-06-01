e901dfe3d7ed169fcdb5ec62e273561f2fe13a20
dluvian <dluvian@proton.me>
2024-06-01 21:42:37 +0800

    chore: Bump version to v0.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.dluvian.voyage"
         minSdk = 26 // Android Oreo (Aug 2017 - Jan 2021)
         targetSdk = 34
-        versionCode = 7
-        versionName = "v0.5.0"
+        versionCode = 8
+        versionName = "v0.5.1"
 
         // Change versionCode, versionName and strings.xml when releasing new
 
