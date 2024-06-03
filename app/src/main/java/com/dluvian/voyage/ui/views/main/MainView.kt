7533e90454c810a2b30192a9a64c5ac4e167cb72
dluvian <dluvian@proton.me>
2024-06-03 16:48:23 +0800

    style: Show account icon in profile row

--- app/src/main/java/com/dluvian/voyage/ui/views/main/MainView.kt
+++ app/src/main/java/com/dluvian/voyage/ui/views/main/MainView.kt
@@ -1,8 +1,11 @@
 package com.dluvian.voyage.ui.views.main
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
@@ -12,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -30,6 +34,7 @@ import com.dluvian.voyage.core.navigator.InboxNavView
 import com.dluvian.voyage.core.navigator.MainNavView
 import com.dluvian.voyage.ui.components.FullHorizontalDivider
 import com.dluvian.voyage.ui.components.scaffold.MainScaffold
+import com.dluvian.voyage.ui.theme.AccountIcon
 import com.dluvian.voyage.ui.theme.spacing
 import com.dluvian.voyage.ui.views.main.subViews.DiscoverView
 import com.dluvian.voyage.ui.views.main.subViews.HomeView
@@ -52,15 +57,22 @@ fun MainView(
                 Spacer(modifier = Modifier.height(spacing.screenEdge))
                 NavigationDrawerItem(
                     label = {
-                        Text(
-                            text = personalProfile.name,
-                            style = TextStyle(
-                                fontSize = 25.sp,
-                                fontWeight = FontWeight.SemiBold
-                            ),
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                imageVector = AccountIcon,
+                                contentDescription = stringResource(id = R.string.open_my_profile)
+                            )
+                            Spacer(modifier = Modifier.width(spacing.medium))
+                            Text(
+                                text = personalProfile.name,
+                                style = TextStyle(
+                                    fontSize = 25.sp,
+                                    fontWeight = FontWeight.SemiBold
+                                ),
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis
+                            )
+                        }
                     },
                     selected = false,
                     onClick = {
