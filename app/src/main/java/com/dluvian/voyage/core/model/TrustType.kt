7930ca945651876420a1aed8d51cab1d6017e86d
dluvian <dluvian@proton.me>
2024-06-06 12:10:12 +0800

    perf: Make TrustType creation stable for UI

--- app/src/main/java/com/dluvian/voyage/core/model/TrustType.kt
+++ app/src/main/java/com/dluvian/voyage/core/model/TrustType.kt
@@ -1,7 +1,12 @@
 package com.dluvian.voyage.core.model
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
+
+@Immutable
 sealed class TrustType {
     companion object {
+        @Stable
         fun from(isOneself: Boolean, isFriend: Boolean, isWebOfTrust: Boolean): TrustType {
             return if (isOneself) Oneself
             else if (isFriend) FriendTrust
