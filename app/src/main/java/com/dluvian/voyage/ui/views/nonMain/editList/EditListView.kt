35a1a3fc071a89760802646d3ec191dc49b95c28
dluvian <dluvian@proton.me>
2024-06-10 19:16:05 +0800

    wip: Prepare adding to list

--- app/src/main/java/com/dluvian/voyage/ui/views/nonMain/editList/EditListView.kt
+++ app/src/main/java/com/dluvian/voyage/ui/views/nonMain/editList/EditListView.kt
@@ -3,14 +3,20 @@ package com.dluvian.voyage.ui.views.nonMain.editList
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.window.Dialog
 import com.dluvian.voyage.R
 import com.dluvian.voyage.core.ComposableContent
 import com.dluvian.voyage.core.Fn
@@ -22,7 +28,9 @@ import com.dluvian.voyage.ui.components.button.RemoveIconButton
 import com.dluvian.voyage.ui.components.icon.TrustIcon
 import com.dluvian.voyage.ui.components.indicator.ComingSoon
 import com.dluvian.voyage.ui.components.row.ClickableRow
+import com.dluvian.voyage.ui.theme.AddIcon
 import com.dluvian.voyage.ui.theme.HashtagIcon
+import kotlinx.coroutines.launch
 
 
 private typealias ItemProps = Triple<ComposableContent, String, Fn>
@@ -75,27 +83,45 @@ private fun ScreenContent(
     topics: List<ItemProps>,
     vm: EditListViewModel
 ) {
+    val profileState = rememberLazyListState()
+    val topicState = rememberLazyListState()
+    val scope = rememberCoroutineScope()
+    val showProfileDialog = remember { mutableStateOf(false) }
+    val showTopicDialog = remember { mutableStateOf(false) }
+    if (showProfileDialog.value) {
+        Dialog(onDismissRequest = { showProfileDialog.value = false }) {
+            Text("lol")
+        }
+    }
+    if (showTopicDialog.value) {
+        Dialog(onDismissRequest = { showTopicDialog.value = false }) {
+            Text("lmao")
+        }
+    }
     SimpleTabPager(
         headers = headers,
         index = vm.tabIndex,
         pagerState = vm.pagerState,
         onScrollUp = {
             when (it) {
-                0 -> {
-                    TODO()
-                }
-
-                1 -> {
-                    TODO()
-                }
-
+                0 -> scope.launch { profileState.animateScrollToItem(0) }
+                1 -> scope.launch { topicState.animateScrollToItem(0) }
                 else -> {}
             }
         },
     ) {
         when (it) {
-            0 -> ItemList(items = profiles)
-            1 -> ItemList(items = topics)
+            0 -> ItemList(
+                items = profiles,
+                addHeader = stringResource(id = R.string.add_profile),
+                state = profileState,
+                onClickAdd = { showProfileDialog.value = true })
+
+            1 -> ItemList(
+                items = topics,
+                addHeader = stringResource(id = R.string.add_topic),
+                state = topicState,
+                onClickAdd = { showTopicDialog.value = true })
 
             else -> ComingSoon()
         }
@@ -103,8 +129,16 @@ private fun ScreenContent(
 }
 
 @Composable
-private fun ItemList(items: List<ItemProps>) {
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
+private fun ItemList(
+    items: List<ItemProps>,
+    addHeader: String,
+    state: LazyListState,
+    onClickAdd: Fn
+) {
+    LazyColumn(modifier = Modifier.fillMaxSize(), state = state) {
+        item {
+            AddRow(header = addHeader, onClick = onClickAdd)
+        }
         items(items) { (icon, label, onRemove) ->
             ClickableRow(
                 header = label,
@@ -116,4 +150,15 @@ private fun ItemList(items: List<ItemProps>) {
             )
         }
     }
+}
+
+@Composable
+private fun AddRow(header: String, onClick: Fn) {
+    ClickableRow(
+        header = header,
+        leadingContent = {
+            Icon(imageVector = AddIcon, contentDescription = null)
+        },
+        onClick = onClick
+    )
 }
