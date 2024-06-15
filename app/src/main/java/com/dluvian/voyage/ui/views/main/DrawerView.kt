750345a01586fa99ebc96426d969e1cb217965b2
dluvian <dluvian@proton.me>
2024-06-15 15:10:56 +0800

    imp: Make drawer scrollable

--- app/src/main/java/com/dluvian/voyage/ui/views/main/DrawerView.kt
+++ app/src/main/java/com/dluvian/voyage/ui/views/main/DrawerView.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
@@ -61,68 +63,82 @@ fun MainDrawer(
                 LaunchedEffect(key1 = vm.drawerState.isOpen) {
                     if (vm.drawerState.isOpen) onUpdate(DrawerViewSubscribeSets)
                 }
-                Spacer(modifier = Modifier.height(spacing.screenEdge))
-                DrawerItem(
-                    label = personalProfile.name,
-                    icon = AccountIcon,
-                    style = TextStyle(
-                        fontSize = 25.sp,
-                        fontWeight = FontWeight.SemiBold
-                    ),
-                    onClick = {
-                        onUpdate(
-                            OpenProfile(nprofile = createNprofile(hex = personalProfile.pubkey))
+                LazyColumn {
+                    item { Spacer(modifier = Modifier.height(spacing.screenEdge)) }
+                    item {
+                        DrawerItem(
+                            label = personalProfile.name,
+                            icon = AccountIcon,
+                            style = TextStyle(
+                                fontSize = 25.sp,
+                                fontWeight = FontWeight.SemiBold
+                            ),
+                            onClick = {
+                                onUpdate(
+                                    OpenProfile(nprofile = createNprofile(hex = personalProfile.pubkey))
+                                )
+                                onUpdate(CloseDrawer(scope = scope))
+                            }
                         )
-                        onUpdate(CloseDrawer(scope = scope))
                     }
-                )
-                DrawerItem(
-                    label = stringResource(id = R.string.follow_lists),
-                    icon = ListIcon,
-                    onClick = {
-                        onUpdate(ClickFollowLists)
-                        onUpdate(CloseDrawer(scope = scope))
+                    item {
+                        DrawerItem(
+                            label = stringResource(id = R.string.follow_lists),
+                            icon = ListIcon,
+                            onClick = {
+                                onUpdate(ClickFollowLists)
+                                onUpdate(CloseDrawer(scope = scope))
+                            }
+                        )
                     }
-                )
-                DrawerItem(
-                    label = stringResource(id = R.string.bookmarks),
-                    icon = BookmarksIcon,
-                    onClick = {
-                        onUpdate(ClickBookmarks)
-                        onUpdate(CloseDrawer(scope = scope))
+                    item {
+                        DrawerItem(
+                            label = stringResource(id = R.string.bookmarks),
+                            icon = BookmarksIcon,
+                            onClick = {
+                                onUpdate(ClickBookmarks)
+                                onUpdate(CloseDrawer(scope = scope))
+                            }
+                        )
                     }
-                )
-                DrawerItem(
-                    label = stringResource(id = R.string.relays),
-                    icon = RelayIcon,
-                    onClick = {
-                        onUpdate(ClickRelayEditor)
-                        onUpdate(CloseDrawer(scope = scope))
+                    item {
+                        DrawerItem(
+                            label = stringResource(id = R.string.relays),
+                            icon = RelayIcon,
+                            onClick = {
+                                onUpdate(ClickRelayEditor)
+                                onUpdate(CloseDrawer(scope = scope))
+                            }
+                        )
                     }
-                )
-                HorizontalDivider(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = spacing.medium)
-                )
-                for (itemSet in itemSets) {
-                    DrawerItem(
-                        label = itemSet.title,
-                        icon = ViewListIcon,
-                        onClick = {
-                            onUpdate(OpenList(identifier = itemSet.identifier))
-                            onUpdate(CloseDrawer(scope = scope))
-                        }
-                    )
+                    item {
+                        HorizontalDivider(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = spacing.medium)
+                        )
+                    }
+                    items(itemSets) {
+                        DrawerItem(
+                            label = it.title,
+                            icon = ViewListIcon,
+                            onClick = {
+                                onUpdate(OpenList(identifier = it.identifier))
+                                onUpdate(CloseDrawer(scope = scope))
+                            }
+                        )
+                    }
+                    item {
+                        DrawerItem(
+                            label = stringResource(id = R.string.create_a_list),
+                            icon = AddIcon,
+                            onClick = {
+                                onUpdate(ClickCreateList)
+                                onUpdate(CloseDrawer(scope = scope))
+                            }
+                        )
+                    }
                 }
-                DrawerItem(
-                    label = stringResource(id = R.string.create_a_list),
-                    icon = AddIcon,
-                    onClick = {
-                        onUpdate(ClickCreateList)
-                        onUpdate(CloseDrawer(scope = scope))
-                    }
-                )
             }
         }
     ) {
