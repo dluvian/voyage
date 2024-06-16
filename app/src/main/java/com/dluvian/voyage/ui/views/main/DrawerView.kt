e43b946c29914e8d63a3a819304b8345dc99e3c9
dluvian <dluvian@proton.me>
2024-06-16 16:51:43 +0800

    wip: Prepare deleting lists

--- app/src/main/java/com/dluvian/voyage/ui/views/main/DrawerView.kt
+++ app/src/main/java/com/dluvian/voyage/ui/views/main/DrawerView.kt
@@ -1,11 +1,17 @@
 package com.dluvian.voyage.ui.views.main
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
@@ -17,6 +23,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
@@ -38,6 +46,7 @@ import com.dluvian.voyage.core.OnUpdate
 import com.dluvian.voyage.core.OpenList
 import com.dluvian.voyage.core.OpenProfile
 import com.dluvian.voyage.core.viewModel.DrawerViewModel
+import com.dluvian.voyage.data.model.ItemSetMeta
 import com.dluvian.voyage.ui.theme.AccountIcon
 import com.dluvian.voyage.ui.theme.AddIcon
 import com.dluvian.voyage.ui.theme.BookmarksIcon
@@ -49,111 +58,88 @@ import kotlinx.coroutines.CoroutineScope
 
 @Composable
 fun MainDrawer(
-    vm: DrawerViewModel,
-    scope: CoroutineScope,
-    onUpdate: OnUpdate,
-    content: ComposableContent
+    vm: DrawerViewModel, scope: CoroutineScope, onUpdate: OnUpdate, content: ComposableContent
 ) {
     val personalProfile by vm.personalProfile.collectAsState()
     val itemSets by vm.itemSetMetas.collectAsState()
-    ModalNavigationDrawer(
-        drawerState = vm.drawerState,
-        drawerContent = {
-            ModalDrawerSheet {
-                LaunchedEffect(key1 = vm.drawerState.isOpen) {
-                    if (vm.drawerState.isOpen) onUpdate(DrawerViewSubscribeSets)
+    ModalNavigationDrawer(drawerState = vm.drawerState, drawerContent = {
+        ModalDrawerSheet {
+            LaunchedEffect(key1 = vm.drawerState.isOpen) {
+                if (vm.drawerState.isOpen) onUpdate(DrawerViewSubscribeSets)
+            }
+            LazyColumn {
+                item { Spacer(modifier = Modifier.height(spacing.screenEdge)) }
+                item {
+                    DrawerItem(label = personalProfile.name, icon = AccountIcon, style = TextStyle(
+                        fontSize = 25.sp, fontWeight = FontWeight.SemiBold
+                    ), onClick = {
+                        onUpdate(
+                            OpenProfile(nprofile = createNprofile(hex = personalProfile.pubkey))
+                        )
+                        onUpdate(CloseDrawer(scope = scope))
+                    })
                 }
-                LazyColumn {
-                    item { Spacer(modifier = Modifier.height(spacing.screenEdge)) }
-                    item {
-                        DrawerItem(
-                            label = personalProfile.name,
-                            icon = AccountIcon,
-                            style = TextStyle(
-                                fontSize = 25.sp,
-                                fontWeight = FontWeight.SemiBold
-                            ),
-                            onClick = {
-                                onUpdate(
-                                    OpenProfile(nprofile = createNprofile(hex = personalProfile.pubkey))
-                                )
-                                onUpdate(CloseDrawer(scope = scope))
-                            }
-                        )
-                    }
-                    item {
-                        DrawerItem(
-                            label = stringResource(id = R.string.follow_lists),
-                            icon = ListIcon,
-                            onClick = {
-                                onUpdate(ClickFollowLists)
-                                onUpdate(CloseDrawer(scope = scope))
-                            }
-                        )
-                    }
-                    item {
-                        DrawerItem(
-                            label = stringResource(id = R.string.bookmarks),
-                            icon = BookmarksIcon,
-                            onClick = {
-                                onUpdate(ClickBookmarks)
-                                onUpdate(CloseDrawer(scope = scope))
-                            }
-                        )
-                    }
-                    item {
-                        DrawerItem(
-                            label = stringResource(id = R.string.relays),
-                            icon = RelayIcon,
-                            onClick = {
-                                onUpdate(ClickRelayEditor)
-                                onUpdate(CloseDrawer(scope = scope))
-                            }
-                        )
-                    }
-                    item {
-                        HorizontalDivider(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(vertical = spacing.medium)
-                        )
-                    }
-                    items(itemSets) {
-                        DrawerItem(
-                            label = it.title,
-                            icon = ViewListIcon,
-                            onClick = {
-                                onUpdate(OpenList(identifier = it.identifier))
-                                onUpdate(CloseDrawer(scope = scope))
-                            }
-                        )
-                    }
-                    item {
-                        DrawerItem(
-                            label = stringResource(id = R.string.create_a_list),
-                            icon = AddIcon,
-                            onClick = {
-                                onUpdate(ClickCreateList)
-                                onUpdate(CloseDrawer(scope = scope))
-                            }
-                        )
-                    }
+                item {
+                    DrawerItem(label = stringResource(id = R.string.follow_lists),
+                        icon = ListIcon,
+                        onClick = {
+                            onUpdate(ClickFollowLists)
+                            onUpdate(CloseDrawer(scope = scope))
+                        })
+                }
+                item {
+                    DrawerItem(label = stringResource(id = R.string.bookmarks),
+                        icon = BookmarksIcon,
+                        onClick = {
+                            onUpdate(ClickBookmarks)
+                            onUpdate(CloseDrawer(scope = scope))
+                        })
+                }
+                item {
+                    DrawerItem(
+                        label = stringResource(id = R.string.relays),
+                        icon = RelayIcon,
+                        onClick = {
+                            onUpdate(ClickRelayEditor)
+                            onUpdate(CloseDrawer(scope = scope))
+                        })
+                }
+                item {
+                    HorizontalDivider(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = spacing.medium)
+                    )
+                }
+                items(itemSets) {
+                    DrawerListItem(itemSetMeta = it, scope = scope, onUpdate = onUpdate)
+                }
+                item {
+                    DrawerItem(label = stringResource(id = R.string.create_a_list),
+                        icon = AddIcon,
+                        onClick = {
+                            onUpdate(ClickCreateList)
+                            onUpdate(CloseDrawer(scope = scope))
+                        })
                 }
             }
         }
-    ) {
+    }) {
         content()
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun DrawerItem(
     label: String,
     icon: ImageVector,
     style: TextStyle = LocalTextStyle.current,
-    onClick: Fn
+    onClick: Fn,
+    onLongClick: Fn = {},
 ) {
     NavigationDrawerItem(
+        modifier = Modifier.combinedClickable(onLongClick = onLongClick, onClick = onClick),
         icon = {
             Icon(imageVector = icon, contentDescription = null)
         },
@@ -163,4 +149,47 @@ private fun DrawerItem(
         selected = false,
         onClick = onClick
     )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun DrawerListItem(itemSetMeta: ItemSetMeta, scope: CoroutineScope, onUpdate: OnUpdate) {
+    val showMenu = remember { mutableStateOf(false) }
+    Box {
+        ItemSetOptionsMenu(
+            isExpanded = showMenu.value,
+            onDismiss = { showMenu.value = false },
+            onClick = {} // TODO
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .combinedClickable(
+                    onLongClick = { showMenu.value = true },
+                    onClick = {
+                        onUpdate(OpenList(identifier = itemSetMeta.identifier))
+                        onUpdate(CloseDrawer(scope = scope))
+                    }
+                ),
+        ) {
+            Icon(imageVector = ViewListIcon, contentDescription = null)
+            Text(text = itemSetMeta.title, maxLines = 1, overflow = TextOverflow.Ellipsis)
+        }
+    }
+}
+
+@Composable
+private fun ItemSetOptionsMenu(
+    isExpanded: Boolean,
+    onDismiss: Fn,
+    onClick: Fn
+) {
+    DropdownMenu(
+        expanded = isExpanded,
+        onDismissRequest = onDismiss
+    ) {
+        DropdownMenuItem(
+            text = { Text(text = stringResource(R.string.delete_list)) }, onClick = onClick
+        )
+    }
 }
