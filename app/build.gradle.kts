06275693fbe958790bd82cf406f2cb59831a8fef
dluvian <dluvian@proton.me>
2024-06-22 12:05:14 +0800

    chore: Upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,17 +71,17 @@ dependencies {
     implementation(project(":nostr-kt"))
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.activity:activity-compose:1.9.0")
 
-    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
     implementation("androidx.security:security-crypto-ktx:1.1.0-alpha06")
 
     val roomVersion = "2.6.1"
