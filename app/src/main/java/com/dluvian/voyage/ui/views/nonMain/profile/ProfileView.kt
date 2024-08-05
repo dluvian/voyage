13d791938510b7fec38b9ebc711dab62c1b9da87
dluvian <dluvian@proton.me>
2024-08-05 18:09:12 +0200

    fix: Don't show semi-trust source when profile is followed

--- app/src/main/java/com/dluvian/voyage/ui/views/nonMain/profile/ProfileView.kt
+++ app/src/main/java/com/dluvian/voyage/ui/views/nonMain/profile/ProfileView.kt
@@ -134,7 +134,7 @@ fun ProfileView(vm: ProfileViewModel, snackbar: SnackbarHostState, onUpdate: OnU
                         ).toBech32()
                     },
                     lightning = profile.lightning,
-                    trustedBy = if (profile.inner.isWebOfTrust) {
+                    trustedBy = if (profile.inner.isWebOfTrust && !profile.inner.isFriend) {
                         vm.trustedBy.value.collectAsState().value
                     } else {
                         null
