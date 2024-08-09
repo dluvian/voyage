4be1b49a3d4df89b8c58712a04add0d97b3efece
dluvian <dluvian@proton.me>
2024-08-09 18:03:53 +0200

    wip: Prepare home feed dao

--- app/src/main/java/com/dluvian/voyage/data/model/TopicSelection.kt
+++ app/src/main/java/com/dluvian/voyage/data/model/TopicSelection.kt
@@ -1,6 +1,14 @@
 package com.dluvian.voyage.data.model
 
-sealed class TopicSelection
+sealed class TopicSelection {
+    fun isMyTopics(): Boolean {
+        return when (this) {
+            MyTopics -> true
+            NoTopics, is ListTopics -> false
+        }
+    }
+}
+
 data object NoTopics : TopicSelection()
 data object MyTopics : TopicSelection()
 data class ListTopics(val identifier: String) : TopicSelection()
--- app/src/main/java/com/dluvian/voyage/data/room/dao/HomeFeedDao.kt
+++ app/src/main/java/com/dluvian/voyage/data/room/dao/HomeFeedDao.kt
@@ -1,10 +1,20 @@
 package com.dluvian.voyage.data.room.dao
 
+import android.util.Log
 import androidx.room.Dao
-import androidx.room.Query
+import com.dluvian.voyage.data.model.CustomPubkeys
+import com.dluvian.voyage.data.model.FriendPubkeys
+import com.dluvian.voyage.data.model.Global
 import com.dluvian.voyage.data.model.HomeFeedSetting
+import com.dluvian.voyage.data.model.ListPubkeys
+import com.dluvian.voyage.data.model.NoPubkeys
+import com.dluvian.voyage.data.model.SingularPubkey
+import com.dluvian.voyage.data.model.WebOfTrustPubkeys
 import com.dluvian.voyage.data.room.view.RootPostView
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+private const val TAG = "HomeFeedDao"
 
 private const val HOME_FEED_BASE_QUERY = "FROM RootPostView " +
         "WHERE createdAt <= :until " +
@@ -22,31 +32,160 @@ private const val HOME_FEED_EXISTS_QUERY = "SELECT EXISTS($HOME_FEED_QUERY)"
 
 @Dao
 interface HomeFeedDao {
-    @Query(HOME_FEED_QUERY)
+
     fun getHomeRootPostFlow(
         setting: HomeFeedSetting,
         until: Long,
         size: Int
-    ): Flow<List<RootPostView>>
+    ): Flow<List<RootPostView>> {
+        val withMyTopics = setting.topicSelection.isMyTopics()
 
-    @Query(HOME_FEED_QUERY)
+        return when (setting.pubkeySelection) {
+            NoPubkeys -> if (withMyTopics) {
+                internalGetTopicFlow()
+            } else {
+                flowOf(emptyList())
+            }
+
+            FriendPubkeys -> if (withMyTopics) {
+                internalGetFriendAndTopicFlow()
+            } else {
+                internalGetFriendFlow()
+            }
+
+            WebOfTrustPubkeys -> if (withMyTopics) {
+                internalGetWotAndTopicFlow()
+            } else {
+                internalGetWotFlow()
+            }
+
+            Global -> if (withMyTopics) {
+                internalGetGlobalAndTopicFlow()
+            } else {
+                internalGetGlobalFlow()
+            }
+
+            is CustomPubkeys, is ListPubkeys, is SingularPubkey -> {
+                Log.w(TAG, "Selection ${setting.pubkeySelection} is not supported")
+                flowOf(emptyList())
+            }
+        }
+    }
+
     suspend fun getHomeRootPosts(
         setting: HomeFeedSetting,
         until: Long,
         size: Int
-    ): List<RootPostView>
+    ): List<RootPostView> {
+        val withMyTopics = setting.topicSelection.isMyTopics()
 
-    @Query(HOME_FEED_EXISTS_QUERY)
+        return when (setting.pubkeySelection) {
+            NoPubkeys -> if (withMyTopics) {
+                internalGetTopic()
+            } else {
+                emptyList()
+            }
+
+            FriendPubkeys -> if (withMyTopics) {
+                internalGetFriendAndTopic()
+            } else {
+                internalGetFriend()
+            }
+
+            WebOfTrustPubkeys -> if (withMyTopics) {
+                internalGetWotAndTopic()
+            } else {
+                internalGetWot()
+            }
+
+            Global -> if (withMyTopics) {
+                internalGetGlobalAndTopic()
+            } else {
+                internalGetGlobal()
+            }
+
+            is CustomPubkeys, is ListPubkeys, is SingularPubkey -> {
+                Log.w(TAG, "Selection ${setting.pubkeySelection} is not supported")
+                emptyList()
+            }
+        }
+    }
+
     fun hasHomeRootPostsFlow(
         setting: HomeFeedSetting,
         until: Long = Long.MAX_VALUE,
         size: Int = 1
-    ): Flow<Boolean>
+    ): Flow<Boolean> {
+        val withMyTopics = setting.topicSelection.isMyTopics()
 
-    @Query(HOME_FEED_CREATED_AT_QUERY)
+        return when (setting.pubkeySelection) {
+            NoPubkeys -> if (withMyTopics) {
+                internalHasTopic()
+            } else {
+                flowOf(false)
+            }
+
+            FriendPubkeys -> if (withMyTopics) {
+                internalHasFriendOrTopic()
+            } else {
+                internalHasFriend()
+            }
+
+            WebOfTrustPubkeys -> if (withMyTopics) {
+                internalHasWotOrTopic()
+            } else {
+                internalHasWot()
+            }
+
+            Global -> if (withMyTopics) {
+                internalHasGlobalOrTopic()
+            } else {
+                internalHasGlobal()
+            }
+
+            is CustomPubkeys, is ListPubkeys, is SingularPubkey -> {
+                Log.w(TAG, "Selection ${setting.pubkeySelection} is not supported")
+                flowOf(false)
+            }
+        }
+    }
+
     suspend fun getHomeRootPostsCreatedAt(
         setting: HomeFeedSetting,
         until: Long,
         size: Int
-    ): List<Long>
+    ): List<Long> {
+        val withMyTopics = setting.topicSelection.isMyTopics()
+
+        return when (setting.pubkeySelection) {
+            NoPubkeys -> if (withMyTopics) {
+                internalGetTopicCreatedAt()
+            } else {
+                emptyList()
+            }
+
+            FriendPubkeys -> if (withMyTopics) {
+                internalGetFriendAndTopicCreatedAt()
+            } else {
+                internalGetFriendCreatedAt()
+            }
+
+            WebOfTrustPubkeys -> if (withMyTopics) {
+                internalGetWotAndTopicCreatedAt()
+            } else {
+                internalGetWotCreatedAt()
+            }
+
+            Global -> if (withMyTopics) {
+                internalGetGlobalAndTopicCreatedAt()
+            } else {
+                internalGetGlobalCreatedAt()
+            }
+
+            is CustomPubkeys, is ListPubkeys, is SingularPubkey -> {
+                Log.w(TAG, "Selection ${setting.pubkeySelection} is not supported")
+                emptyList()
+            }
+        }
+    }
 }
