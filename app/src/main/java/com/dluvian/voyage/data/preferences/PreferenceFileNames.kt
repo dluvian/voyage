d1bd85a29ba2713f35cdc1cf213196ce4b3b3ca5
dluvian <dluvian@proton.me>
2024-08-11 16:43:24 +0200

    fix: Rename relay file

--- app/src/main/java/com/dluvian/voyage/data/preferences/PreferenceFileNames.kt
+++ app/src/main/java/com/dluvian/voyage/data/preferences/PreferenceFileNames.kt
@@ -3,4 +3,4 @@ package com.dluvian.voyage.data.preferences
 const val DATABASE_FILE: String = "database"
 const val EVENT_FILE: String = "event"
 const val HOME_FEED_FILE: String = "home_feed"
-const val RELAY_FILE: String = "home_feed"
+const val RELAY_FILE: String = "relay_feed"
