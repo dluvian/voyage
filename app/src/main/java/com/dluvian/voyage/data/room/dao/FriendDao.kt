2a7e9d7c2820ecf8cfe4997ffe621f552584cbe9
dluvian <dluvian@proton.me>
2024-08-21 16:37:18 +0200

    wip: Stop subbing stuff from locked users

--- app/src/main/java/com/dluvian/voyage/data/room/dao/FriendDao.kt
+++ app/src/main/java/com/dluvian/voyage/data/room/dao/FriendDao.kt
@@ -5,22 +5,35 @@ import androidx.room.Query
 import com.dluvian.voyage.core.PubkeyHex
 import kotlinx.coroutines.flow.Flow
 
-// TODO: Exclude locks in other methods
-
 @Dao
 interface FriendDao {
     @Query("SELECT friendPubkey FROM friend WHERE friendPubkey NOT IN (SELECT pubkey FROM lock)")
     fun getFriendsNoLockFlow(): Flow<List<PubkeyHex>>
 
-    @Query("SELECT friendPubkey FROM friend WHERE friendPubkey NOT IN (SELECT friendPubkey FROM weboftrust)")
+    @Query(
+        "SELECT friendPubkey " +
+                "FROM friend " +
+                "WHERE friendPubkey NOT IN (SELECT friendPubkey FROM weboftrust) " +
+                "AND friendPubkey NOT IN (SELECT pubkey FROM lock)"
+    )
     suspend fun getFriendsWithMissingContactList(): List<PubkeyHex>
 
-    @Query("SELECT friendPubkey FROM friend WHERE friendPubkey NOT IN (SELECT pubkey FROM nip65)")
+    @Query(
+        "SELECT friendPubkey " +
+                "FROM friend " +
+                "WHERE friendPubkey NOT IN (SELECT pubkey FROM nip65) " +
+                "AND friendPubkey NOT IN (SELECT pubkey FROM lock)"
+    )
     suspend fun getFriendsWithMissingNip65(): List<PubkeyHex>
 
-    @Query("SELECT friendPubkey FROM friend WHERE friendPubkey NOT IN (SELECT pubkey FROM profile)")
+    @Query(
+        "SELECT friendPubkey " +
+                "FROM friend " +
+                "WHERE friendPubkey NOT IN (SELECT pubkey FROM profile) " +
+                "AND friendPubkey NOT IN (SELECT pubkey FROM lock)"
+    )
     suspend fun getFriendsWithMissingProfile(): List<PubkeyHex>
 
-    @Query("SELECT MAX(createdAt) FROM friend")
+    @Query("SELECT MAX(createdAt) FROM friend WHERE friendPubkey NOT IN (SELECT pubkey FROM lock)")
     suspend fun getMaxCreatedAt(): Long?
 }
