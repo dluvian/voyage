38c7bc2e65fba80bbcb152fdb490c21fba007dcf
dluvian <dluvian@proton.me>
2024-10-03 19:44:26 -0600

    chore: Upgrade compose to 2024.09.03

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("androidx.activity:activity-compose:1.9.2")
 
-    implementation(platform("androidx.compose:compose-bom:2024.09.02"))
+    implementation(platform("androidx.compose:compose-bom:2024.09.03"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
