c7560a7599bb2dcb55d25acc1ca29e418f4741a3
dluvian <dluvian@proton.me>
2024-10-16 17:00:39 -0600

    wip(ui): Prepare poll

--- app/src/main/java/com/dluvian/voyage/ui/components/row/mainEvent/MainEventRow.kt
+++ app/src/main/java/com/dluvian/voyage/ui/components/row/mainEvent/MainEventRow.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -12,14 +13,21 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.dluvian.voyage.core.ComposableContent
+import com.dluvian.voyage.core.Fn
 import com.dluvian.voyage.core.MAX_CONTENT_LINES
 import com.dluvian.voyage.core.MAX_SUBJECT_LINES
 import com.dluvian.voyage.core.OnUpdate
@@ -34,6 +42,7 @@ import com.dluvian.voyage.core.model.Poll
 import com.dluvian.voyage.core.model.RootPost
 import com.dluvian.voyage.core.model.ThreadableMainEvent
 import com.dluvian.voyage.data.nostr.createNevent
+import com.dluvian.voyage.ui.components.FullHorizontalDivider
 import com.dluvian.voyage.ui.components.button.OptionsButton
 import com.dluvian.voyage.ui.components.button.footer.CountedCommentButton
 import com.dluvian.voyage.ui.components.button.footer.ReplyIconButton
@@ -211,12 +220,92 @@ private fun RowWithDivider(level: Int, content: ComposableContent) {
 
 @Composable
 private fun PollColumn(poll: Poll) {
+    val clickedId = remember {
+        mutableStateOf<String?>(null)
+    }
     val alreadyVoted = remember(poll) {
-        poll.options.any { it.isMyVote }
+        mutableStateOf(poll.options.any { it.isMyVote })
+    }
+    val topVotes = remember(poll) {
+        poll.options.maxOf { it.voteCount }
+        42
+    }
+    val totalVotes = remember(poll) {
+        poll.options.sumOf { it.voteCount }
+        50
     }
     Column {
         for (option in poll.options) {
-            Text("${option.optionId}: ${option.label}")
+            PollOptionRow(
+                label = option.label,
+                isSelected = if (clickedId.value != null) clickedId.value == option.optionId else option.isMyVote,
+                isRevealed = alreadyVoted.value,
+                percentage = if (option.optionId == clickedId.value) 100 else remember(
+                    option.voteCount,
+                    totalVotes
+                ) {
+                    if (totalVotes == 0) 0
+                    else option.voteCount.div(totalVotes).times(100)
+                },
+                progress = if (option.optionId == clickedId.value) 1f else remember(
+                    option.voteCount,
+                    topVotes
+                ) {
+                    if (topVotes == 0) 0f else option.voteCount.toFloat().div(topVotes)
+                },
+                onClick = {
+                    if (clickedId.value == option.optionId && alreadyVoted.value) {
+                        alreadyVoted.value = false
+                    } else if (clickedId.value == option.optionId) {
+                        alreadyVoted.value = true
+                    }
+                    clickedId.value = option.optionId
+                }
+            )
+        }
+        Text(
+            modifier = Modifier.padding(start = spacing.large),
+            text = if (totalVotes == 0) "No votes" else "$totalVotes votes"
+        )
+    }
+}
+
+@Composable
+private fun PollOptionRow(
+    label: String,
+    isSelected: Boolean,
+    isRevealed: Boolean,
+    percentage: Int,
+    progress: Float,
+    onClick: Fn
+) {
+    Column {
+        Row(
+            modifier = Modifier
+                .clickable(onClick = onClick)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(
+                modifier = Modifier.width(42.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                if (isRevealed) Text(
+                    modifier = Modifier.padding(vertical = spacing.xl),
+                    text = "$percentage%"
+                )
+                else RadioButton(selected = isSelected, onClick = onClick)
+            }
+            Spacer(modifier = Modifier.padding(start = spacing.medium))
+            Text(text = label)
+        }
+        Row {
+            Spacer(modifier = Modifier.width(spacing.medium))
+            if (isRevealed) LinearProgressIndicator(
+                modifier = Modifier.fillMaxWidth(),
+                progress = { progress })
+            else FullHorizontalDivider()
         }
     }
 }
