918b995718678a04a654bb5422b2fea85a6a6cc6
dluvian <dluvian@proton.me>
2024-10-22 16:08:06 -0600

    ui: Fix text style

--- app/src/main/java/com/dluvian/voyage/ui/components/text/AnnotatedText.kt
+++ app/src/main/java/com/dluvian/voyage/ui/components/text/AnnotatedText.kt
@@ -3,11 +3,9 @@ package com.dluvian.voyage.ui.components.text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextOverflow
 
 @Composable
@@ -17,22 +15,11 @@ fun AnnotatedText(
     style: TextStyle = MaterialTheme.typography.bodyLarge,
     maxLines: Int = Int.MAX_VALUE,
 ) {
-    val color = MaterialTheme.colorScheme.onSurface
-    val annotatedString = remember(text, style) {
-        useDefaultTextStyle(text = text, style = style.copy(color = color))
-    }
     Text(
         modifier = modifier,
-        text = annotatedString,
+        text = text,
         maxLines = maxLines,
         overflow = TextOverflow.Ellipsis,
+        style = style.copy(color = MaterialTheme.colorScheme.onSurface),
     )
-}
-
-private fun useDefaultTextStyle(text: AnnotatedString, style: TextStyle): AnnotatedString {
-    return buildAnnotatedString {
-        val index = pushStyle(style.toSpanStyle())
-        append(text)
-        pop(index)
-    }
 }
