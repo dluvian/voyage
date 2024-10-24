03b2f29393781c2125eeb91cc124d8c93e9d3751
dluvian <dluvian@proton.me>
2024-10-24 17:44:29 -0600

    chore: Upgrade ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.26" apply false
     id("com.android.library") version "8.5.2" apply false
 }
