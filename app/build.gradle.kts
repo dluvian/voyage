b1c1573081e3551a09db38c6cc1a77643eeaa855
dluvian <dluvian@proton.me>
2025-05-25 16:07:31 +0800

    chore: Upgrade compose bom

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,7 +84,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
     implementation("androidx.activity:activity-compose:1.10.1")
 
-    implementation(platform("androidx.compose:compose-bom:2025.05.00"))
+    implementation(platform("androidx.compose:compose-bom:2025.05.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
