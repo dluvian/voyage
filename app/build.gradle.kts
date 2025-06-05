1cc26ff17871bf0ee6109e271b1e151eba59965c
dluvian <dluvian@proton.me>
2025-06-05 10:57:19 +0800

    chore: Upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,18 +86,18 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
     implementation("androidx.activity:activity-compose:1.10.1")
 
-    implementation(platform("androidx.compose:compose-bom:2025.05.01"))
+    implementation(platform("androidx.compose:compose-bom:2025.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
-    implementation("androidx.security:security-crypto-ktx:1.1.0-alpha07")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.1")
+    implementation("androidx.security:security-crypto-ktx:1.1.0-beta01")
 
     implementation("org.rust-nostr:nostr-sdk:0.42.0")
     implementation("cash.z.ecc.android:kotlin-bip39:1.0.9")
